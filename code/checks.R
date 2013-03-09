8be3f60ed8e480c4baa0d1006a02050ba8f64d07
Aaron Schumacher <ajschumacher@gmail.com>
2013-03-09 18:37:43 -0500

    with a pre-aggregated file now

--- code/checks.R
+++ code/checks.R
@@ -5,3 +5,10 @@ test2 <- read.csv('../turnstile2/turnstile_110402.txt', header=FALSE)
 raw <- read.csv('../results.csv')
 sorted <- raw[order(raw$Station, raw$day),]
 write.csv(sorted, '../resultsSorted.csv',row.names=FALSE)
+
+daily <- aggregate(entries ~ day, data=raw, FUN="sum")
+daily$date <- as.Date(daily$day)
+
+daily$day <- NULL
+
+write.csv(daily, '../resultsDaily.csv', row.names=FALSE)
